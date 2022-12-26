40e3bb13736df88652ef409d1262ca402ce7283d
Jordan Schnur <actjay1898@gmail.com>
2022-12-26 16:25:33 -0500

    Updated linter to fml

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use elasticist::{FlexWrap, Node, Options, Vector2};
 
 fn main() {
-    let options = Options {flex_wrap: FlexWrap::Wrap, ..Default::default()};
+    let options = Options {
+        flex_wrap: FlexWrap::Wrap,
+        ..Default::default()
+    };
     let mut vec = Vector2::zero();
     println!("X: {:?}", vec.x);
     vec.test();
     println!("X2: {:?}", vec.x);
     let mut node = Node::new(Vector2::zero(), Vector2::new(None, None), options);
-    println!("Before Method: Column gap {:?}. Row Gap {:?}", node.options.column_gap, node.options.row_gap);
+    println!(
+        "Before Method: Column gap {:?}. Row Gap {:?}",
+        node.options.column_gap, node.options.row_gap
+    );
     node.options.gap(10);
-    println!("After Method: Column gap {:?}. Row Gap {:?}", node.options.column_gap, node.options.row_gap);
+    println!(
+        "After Method: Column gap {:?}. Row Gap {:?}",
+        node.options.column_gap, node.options.row_gap
+    );
 }
